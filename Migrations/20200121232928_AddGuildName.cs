833ea015d3921c7e10fc2e2dc43700d6f690c36c
exsersewo <exsersewo@systemexit.co.uk>
2020-01-21 23:31:25 +0000

    Add AddGuildName migration

---
+++ Migrations/20200121232928_AddGuildName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skuld.Core.Models.Migrations
+{
+    public partial class AddGuildName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Guilds",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Guilds");
+        }
+    }
+}
