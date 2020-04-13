8569d4afe751e4e8e5f5753534edb3df601028b7
exsersewo <exsersewo@systemexit.co.uk>
2020-04-13 21:25:26 +0100

    Add migration

---
+++ Skuld.Models/Migrations/20200413200816_SeperateVoiceExperience.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Skuld.Models.Migrations
+{
+    public partial class SeperateVoiceExperience : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVoiceExperience",
+                table: "UserXp",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsVoiceExperience",
+                table: "UserXp");
+        }
+    }
+}
