245173a9937e3d7af632d2a639a19b466518e69c
exsersewo <exsersewo@systemexit.co.uk>
2020-04-15 22:20:41 +0100

    Tweak Experience

--- Skuld.Models/DatabaseUtilities.cs
+++ Skuld.Models/DatabaseUtilities.cs
@@ -12,7 +12,11 @@ namespace Skuld.Models
             => (ulong)Math.Round(Math.Clamp((float)Math.Pow(level, growthmod) * 50, 0, 1500000), MidpointRounding.AwayFromZero);
 
         public static ulong GetLevelFromTotalXP(ulong totalxp, double growthmod)
-            => (ulong)Math.Round(Math.Pow(totalxp / 50, 2f).NthRoot(5), MidpointRounding.AwayFromZero);
+        {
+            var split = growthmod.ToString().Split(".");
+
+            return (ulong)Math.Round(Math.Pow(totalxp / 50, Convert.ToDouble(split[0])).NthRoot(Convert.ToInt32(split[1])), MidpointRounding.AwayFromZero);
+        }
 
         /// <summary>
         /// Gets the experience multiplier from Users Minutes in Voice
@@ -36,60 +40,6 @@ namespace Skuld.Models
                 result = 0; //if negative clamp to zero
 
             return (int)Math.Round(result); //return rounded integral version of result
-        }
-
-        public static UserExperience GetJoinedExperience(this IEnumerable<UserExperience> experiences, ulong userId, IGuild guild = null)
-        {
-            UserExperience textExperience;
-            UserExperience voiceExperience;
-
-            if(guild != null)
-            {
-                textExperience = experiences.FirstOrDefault(x => x.GuildId == guild.Id && x.UserId == userId);
-                voiceExperience = experiences.FirstOrDefault(x => x.GuildId == guild.Id && x.IsVoiceExperience == true && x.UserId == userId);
-            }
-            else
-            {
-                textExperience = experiences.FirstOrDefault(x => x.UserId == userId);
-                voiceExperience = experiences.FirstOrDefault(x => x.IsVoiceExperience == true && x.UserId == userId);
-            }
-
-            var lxp = textExperience;
-
-            if(voiceExperience != null)
-            {
-                lxp.TotalXP = lxp.TotalXP.Add(voiceExperience.TotalXP);
-                lxp.XP = lxp.XP.Add(voiceExperience.XP);
-                lxp.Level = lxp.Level.Add(voiceExperience.Level);
-            }
-
-            bool didLevel = false;
-
-            ulong levelAmount = 0;
-
-            var xptonextlevel = GetXPLevelRequirement(lxp.Level + 1, 2.5);
-
-            var currXp = lxp.XP;
-
-            if (lxp.XP >= xptonextlevel)
-            {
-                while (currXp >= xptonextlevel)
-                {
-                    levelAmount++;
-                    currXp = currXp.Subtract(xptonextlevel);
-                    xptonextlevel = GetXPLevelRequirement(lxp.Level + 1 + levelAmount, 2.5);
-
-                    didLevel = true;
-                }
-            }
-
-            if(didLevel)
-            {
-                lxp.XP = currXp;
-                lxp.Level = lxp.Level.Add(levelAmount);
-            }
-
-            return lxp;
         }
     }
 }
